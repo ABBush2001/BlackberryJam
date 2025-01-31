3f40a27e28526a6e22acc12bd9dcfd300b0070b0
Katuna209 <squishyninjafishy@gmail.com>
2025-01-31 01:52:28 -0600

    Code for the two screens
    Functionally identical to the MainMenu script, just with a rename since it has a fit otherwise.

---
+++ Gameover_LevelComplete_Screens/Level Complete.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class LevelComplete: MonoBehaviour
+{
+    // Add this scene to your build. In the LoadScene part, put the main game level index
+	// or the level name string (as "levelname Example") inbetween the parentheses. 
+    public void PlayGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+	
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
